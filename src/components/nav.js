a4c59cf605f21b568dc8f84c87488af97260cf73
nguyenvanliem0501 <nguyenvanliem0501@gmail.com>
2022-01-15 14:53:12 +0700

    update code lab3

--- src/components/nav.js
+++ src/components/nav.js
@@ -12,17 +12,19 @@ const Nav = {
               </ul>
             </nav>
           </div>
+
+          <div>
+            <ul class="flex text-white text-base">
+              <li><a class="lg:inline-flex lg:w-auto px-3 hover:text-slate-300" href="/admin/dashboard">Admin</a></li>
+              <li><a class="lg:inline-flex lg:w-auto px-3 hover:text-slate-300" href="/signup">Đăng ký</a></li>
+              <li><a class="lg:inline-flex lg:w-auto px-3 hover:text-slate-300" href="/sign">Đăng nhập</a></li>
+            </ul>
+          </div>
           <div class="search">
             <input class="py-1" type="text">
             <button class="bg-blue-900 hover:bg-orange-400 px-2 text-base py-1 text-white">Tìm kiếm</button>
           </div>
-          <div>
-            <ul class="flex text-white text-base">
-              <li><a class="lg:inline-flex lg:w-auto px-3 hover:text-slate-300" href="/signup">Đăng ký</a></li>
-              <li><a class="lg:inline-flex lg:w-auto px-3 hover:text-slate-300" href="/sign">Đăng nhập</a></li>
-              <li><a class="lg:inline-flex lg:w-auto px-3 hover:text-slate-300" href="/admin/dashboard">Admin</a></li>
-            </ul>
-          </div>
+          
         </div>`;
     },
 };
--- src/data.js
+++ src/data.js
@@ -1,12 +1,12 @@
 const data = [
     {
-        id: "1", title: "Vinh danh 295 sinh viên FPT Polytechnic học kỳ Spring 2018", img: "https://caodang.fpt.edu.vn/wp-content/uploads/xFPT_polytechnic_ky_summer2020-768x768.png.pagespeed.ic.m3Mx27hr6E.webp", desc: "Tối ngày 16/06/2018, Cao đẳng FPT Polytechnic Hà Nội đã tổ chức lễ tôn vinh Ong vàng Poly, tuyên dương 295 cá nhân và tập thể có thành tích xuất sắc trong học tập cũng như các hoạt động khác.",
+        id: "1", title: "Vinh danh 295 sinh viên FPT Polytechnic học kỳ Spring 2018", img: "https://fpt.edu.vn/Resources/article/uploads/2019/20200515111907377_IMG_2932.JPG", desc: "Tối ngày 16/06/2018, Cao đẳng FPT Polytechnic Hà Nội đã tổ chức lễ tôn vinh Ong vàng Poly, tuyên dương 295 cá nhân và tập thể có thành tích xuất sắc trong học tập cũng như các hoạt động khác.",
     },
     {
-        id: "2", title: "Sinh viên khối ngành Kinh tế học cách 'va chạm' vào cảm xúc", img: "https://caodang.fpt.edu.vn/wp-content/uploads/tham-quan.jpg", desc: "Với sự tham gia của chuyên gia trong lĩnh vực kinh doanh, chương trình đã thu hút đông đảo sinh viên Cao đẳng FPT Polytechnic tham dự. Đặc biệt các bạn sinh viên bị lôi cuốn bởi...",
+        id: "2", title: "Sinh viên khối ngành Kinh tế học cách 'va chạm' vào cảm xúc", img: "https://phothongcaodang.fpt.edu.vn/wp-content/uploads/2019/12/ok4.jpg", desc: "Với sự tham gia của chuyên gia trong lĩnh vực kinh doanh, chương trình đã thu hút đông đảo sinh viên Cao đẳng FPT Polytechnic tham dự. Đặc biệt các bạn sinh viên bị lôi cuốn bởi...",
     },
     {
-        id: "3", title: "Sinh viên FPoly trải nghiệm thực tế tại Mường Thanh Luxury", img: "https://caodang.fpt.edu.vn/wp-content/uploads/IMG_20180608_145330.jpg", desc: "Với triết lý đào tạo “Thực học – Thực nghiêp”, Cao đẳng FPT Polytechnic Đà Nẵng thường xuyên tổ chức cho sinh viên tham quan các đơn vị doanh nghiệp...",
+        id: "3", title: "Sinh viên FPoly trải nghiệm thực tế tại Mường Thanh Luxury", img: "https://vtv1.mediacdn.vn/thumb_w/640/2020/8/20/fpt-polytechnic-1597925474001310106893-crop-1597925488297507918807.jpg", desc: "Với triết lý đào tạo “Thực học – Thực nghiêp”, Cao đẳng FPT Polytechnic Đà Nẵng thường xuyên tổ chức cho sinh viên tham quan các đơn vị doanh nghiệp...",
     },
 ];
 export default data;
